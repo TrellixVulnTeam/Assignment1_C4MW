4b99e9a36600be88ac2926ace8831f9139cb1879
fyaseen <feazany@gmail.com>
2018-02-27 18:50:29 -0500

    Question 5 Part 1

--- q4_p2.py
+++ q4_p2.py
@@ -1,13 +1,15 @@
 from q4_p1 import word_count
 
-articles = open("top10articles.txt", "w")
+top_10_words = open("top10words.txt", "w")
 word_dict = word_count()
+top_word_list = []
+index = 1
 
-
-for key in word_dict:
-    if word_dict[key] > 2:
-        print(key)
-
-print(len(word_dict))
-
-
+if len(word_dict) <= 10:
+    for k, v in sorted(word_dict.items(), key=lambda p: p[1], reverse=True):
+        top_10_words.write(k + ", " + str(v) + "\n")
+else:
+    for k, v in sorted(word_dict.items(), key=lambda p: p[1], reverse=True):
+        if index <= 10:
+            top_10_words.write(k + ", " + str(v) + "\n")
+            index += 1
---
+++ q5_p1.py
@@ -0,0 +1,10 @@
+password = input("Please enter a password: ")
+
+
+def length(the_password):
+    if len(the_password) >= 8:
+        return True
+    else:
+        return False
+
+def numero_alpha(the_password):
