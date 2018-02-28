ce2cc660bf6c6ec41fc5ff9b21d6e3a7c61a58cb
fyaseen <feazany@gmail.com>
2018-02-28 18:49:56 -0500

    Working on Question 6 part 4

--- q6_p4.py
+++ q6_p4.py
@@ -6,10 +6,22 @@
 #
 # print(count_pattern("hello", "yo", "this"))
 
+
 def count_pattern(str, pattern, replace_str):
     assert len(str) >= len(pattern)
-    str = str.replace(str, pattern)
-    return str
+
+    str_list = list(str)
+    pattern_list = list(pattern)
+    replace_str_list = list(replace_str)
+    indexes = []
+
+    # Locate where the pattern is in the original string
+    for i in range(0, len(str_list)):
+        for j in range(0, len(pattern_list)):
+            if str_list[i] == pattern_list[j]:
+                indexes.append(i)
+
+
 
 
 print(count_pattern("hello", "yo", "this"))
