3b4dd5dfcd1c3d3f63c26979afe3a6e7e1257480
fyaseen <feazany@gmail.com>
2018-03-01 15:02:38 -0500

    Working on Question 6 part 4

--- q6_p4.py
+++ q6_p4.py
@@ -1,18 +1,33 @@
+def count_pattern_helper(ord_str):
+    ord_list = []
+    for i in range(0, len(ord_str) - 1):
+        ord_list.append(ord(ord_str[i]) - ord(ord_str[i+1]))
+    return ord_list
+
 def count_pattern(str, pattern, replace_str):
     assert len(str) >= len(pattern)
-
+    ordinance_list = []
+    location_of_pattern = []
     str_list = list(str)
     pattern_list = list(pattern)
     replace_str_list = list(replace_str)
-    indexes = []
 
-    # Locate where the pattern is in the original string
-    for i in range(0, len(str_list)):
-        for j in range(0, len(pattern_list)):
-            if str_list[i] == pattern_list[j]:
-                indexes.append(i)
+    # Find the ordinance of the pattern to check against str
+    ordinance_list = count_pattern_helper(pattern)
+    print(ordinance_list)
+
+    j = 0
+    for i in range(0, len(str_list) - 1):
+        if ord(str_list[i]) - ord(str_list[i+1]) == ordinance_list[j]:
+            # Need to have a check
+            if count_pattern_helper(str_list[i:i + len(pattern)]) == ordinance_list:
+                location_of_pattern.append(str_list[i:i + len(pattern)])
+
+    print(location_of_pattern)
 
 
 
 
-print(count_pattern("hello", "yo", "this"))
+
+
+count_pattern("ahihfdddedaaba", "xyx", "123")
